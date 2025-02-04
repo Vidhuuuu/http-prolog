54bed4ee03010fa81f9a1bf405fa992285e550b9
vidhu <vidhudash@gmail.com>
2025-02-04 12:31:58 +0530

    feat: add media types

--- server/parse_req.pl
+++ server/parse_req.pl
@@ -1,5 +1,9 @@
 :- module(parse_req, [parse_request/2]).
 
+media_type('.html', 'text/html').
+media_type('.txt', 'text/plain').
+media_type(_, 'application/octet-stream').
+
 parse_request(Request, Response) :-
     (Request = "" ->
         Response = "HTTP/1.0 400 Bad Request\r\n\r\n",
@@ -22,13 +26,17 @@ handle_method(_, _, _, Response) :-
 serve_get_req("/", Version, Response) :-
     serve_get_req("/index.html", Version, Response).
 serve_get_req(Path, Version, Response) :-
+    file_name_extension(_, Ext, Path),
+    atom_concat('.', Ext, PeriodExt),
+    media_type(PeriodExt, MediaType),
+
     atom_concat('static', Path, FilePath),
     (exists_file(FilePath) ->
         size_file(FilePath, FileSize),
         read_file_to_string(FilePath, FileContent, []),
         format(string(Response),
-        "~s 200 OK\r\nContent-Type: text/html\r\nContent-Length:~d\r\n\r\n~s",
-            [Version, FileSize, FileContent])
+        "~s 200 OK\r\nContent-Type: ~w\r\nContent-Length:~d\r\n\r\n~s",
+            [Version, MediaType, FileSize, FileContent])
     ;
         Response = "HTTP/1.0 404 File Not Found\r\n\r\n",
         !
