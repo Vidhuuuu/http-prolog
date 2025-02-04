c798f4aa3fb3aabbc17c04ba39a904a2264f6828
vidhu <vidhudash@gmail.com>
2025-02-04 12:20:06 +0530

    feat: handle status code 400

--- server/parse_req.pl
+++ server/parse_req.pl
@@ -1,12 +1,23 @@
 :- module(parse_req, [parse_request/2]).
 
 parse_request(Request, Response) :-
-    split_string(Request, " \r\n", "", [Method, Path, Version | _]),
-    (Method == "GET" ->
-        serve_get_req(Path, Version, Response)
+    (Request = "" ->
+        Response = "HTTP/1.0 400 Bad Request\r\n\r\n",
+        !
     ;
-        Response = "HTTP/1.0 405 Method Not Allowed\r\n\r\n"
+        (split_string(Request, " \r\n", "", [Method, Path, Version | _]) ->
+            handle_method(Method, Path, Version, Response)
+        ;
+            Response = "HTTP/1.0 400 Bad Request\r\n\r\n",
+            !
+        )
     ).
+
+handle_method("GET", Path, Version, Response) :-
+    serve_get_req(Path, Version, Response).
+handle_method(_, _, _, Response) :-
+    Response = "HTTP/1.0 405 Method Not Allowed\r\n\r\n",
+    !
 
 serve_get_req("/", Version, Response) :-
     serve_get_req("/index.html", Version, Response).
@@ -19,6 +30,6 @@ serve_get_req(Path, Version, Response) :-
         "~s 200 OK\r\nContent-Type: text/html\r\nContent-Length:~d\r\n\r\n~s",
             [Version, FileSize, FileContent])
     ;
-        Response = "HTTP/1.0 404 File Not Found\r\n\r\n"
+        Response = "HTTP/1.0 404 File Not Found\r\n\r\n",
+        !
     ).
-
